8d0e303ccf1e3ef8347df51eec2fff85dbc0783c
prakash2903 <lgs.padhmaprakash29@gmail.com>
2023-08-13 21:45:09 +0530

    Initial commit

---
+++ Admin/Includes/footer.php
@@ -0,0 +1,9 @@
+ <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span> &copy; <script> document.write(new Date().getFullYear()); </script> - Developed by PSG COLLEGE OF TECHNOLOGY ~ HITESH& PRAKASH
+             
+            </span>
+          </div>
+        </div>
+      </footer>
---
+++ ClassTeacher/Includes/title.php
@@ -0,0 +1,1 @@
+  <title>PSG - Dashboard</title>
